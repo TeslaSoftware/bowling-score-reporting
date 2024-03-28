5a5bb47d7e72164908e4dae79069b7253e4b44e5
Sebastian <stysler@gmail.com>
2024-03-28 23:53:46 +0100

    Implement calculating the score

--- src/app/parsing-service.service.ts
+++ src/app/parsing-service.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { IBowlingResult } from './ibowling-result'
 import { of, Observable, BehaviorSubject } from 'rxjs';
 
+/** 
+ * Service parsing and validating raw text data into bowling result type. 
+ * In addition service also stores the data containing bowiling results 
+ * and allows other components to access it via its public method.
+ * */
 @Injectable({
   providedIn: 'root'
 })
@@ -22,20 +27,21 @@ export class ParsingService {
     let bowlingResults: IBowlingResult[] = [];
     try {
       bowlingResults = this.parseAndValidateRawText(rawTextData);
-    } catch(error: unknown) {
+    } catch (error: unknown) {
       let errorMessage;
       if (typeof error === "string") {
-        errorMessage = error.toUpperCase() // works, `e` narrowed to string
+        errorMessage = error.toUpperCase()
       } else if (error instanceof Error) {
         errorMessage = error.message
       }
       console.log("Parsing failed due to " + errorMessage);
       return false;
-    } finally{
+    } finally {
       this.bowlingResultsSubject.next(bowlingResults);
     }
-    
+
     console.log("Parsing was successful!!");
+    console.log(bowlingResults);
     return true;
   }
 
@@ -45,6 +51,7 @@ export class ParsingService {
 
   /** 
    * Attempts to parse and validate raw text to expected data format.
+   * 
    * Throws an error when invalid data is encountered.
    */
   private parseAndValidateRawText(rawText: string): IBowlingResult[] {
@@ -60,7 +67,7 @@ export class ParsingService {
       const scores = this.parseAndValidateScoresString(textLines[i + 1]);
       const totalScore = this.calculateTotalScore(scores);
 
-      const bowlingResult:IBowlingResult = {
+      const bowlingResult: IBowlingResult = {
         playerName, scores, totalScore
       };
 
@@ -70,36 +77,78 @@ export class ParsingService {
     return results;
   }
 
-  /** Parses and validates scores string. */
+  /** 
+   * Parses and validates scores from raw text line of scores separated by commas. 
+   * 
+   * Assumes that when player scores strike (10 pins) in first roll of a given frame,
+   * the second roll of this frame will be marked as 0.
+   * Therefore there always will be between 20 and 22 rolls in total.
+   * */
   private parseAndValidateScoresString(scoresLine: string): number[] {
     const scoresStrings = scoresLine.split(",").map(str => str.trim())
 
-    if(scoresStrings.some(item => !this.isNumeric(item))){
+    if (scoresStrings.some(item => !this.isNumeric(item))) {
       throw new Error("Nieprawidłowy format! Przynajmniej jeden z rzutów jest nie jest numerem");
     }
 
     const numericScores = scoresStrings.map(str => parseInt(str));
 
-    if(numericScores.length < 20 || numericScores.length > 21){
-      throw new Error("Nieprawidłowy format. Oczekiwano numer rzutów 20 lub 21 a było " + numericScores.length);
+    if (numericScores.length < 20 || numericScores.length > 22) {
+      throw new Error("Nieprawidłowy format. Oczekiwano numer rzutów między 20 a 22, a było " + numericScores.length);
     }
 
     for (let i = 0; i + 1 < numericScores.length; i += 2) {
       const scoreForBothRolls = numericScores[i] + numericScores[i + 1]
-      if(scoreForBothRolls > 10) {
+      if (scoreForBothRolls > 10) {
         throw new Error("Nieprawidłowy format. W jednej rundzie można maksymalnie zdobyć 10 punktów a zawodnik zdobył " + scoreForBothRolls);
       }
     }
 
-    return numericScores; 
+    return numericScores;
   }
 
-  /** Calculates total score based on the game rules. */
+  /** 
+   * Calculates total score based on the game rules. 
+   * 
+   * Assumes that when player scores strike (10 pins) in first roll of a given frame,
+   * the second roll of this frame will be marked as 0. 
+   * Therefore there always will be between 20 and 22 rolls in total.
+   * */
   private calculateTotalScore(scores: number[]): number {
-    // TODO: implement this menthod
-    return 999;
+    let isStrike = false;
+    let isSpare = false;
+
+    let totalScore = 0;
+
+    for (let i = 0; i < scores.length; i += 2) {
+      const firstRoll = scores[i];
+      const secondRoll = i + 1 == scores.length ? 0 : scores[i + 1];
+
+      totalScore += (firstRoll + secondRoll);
+
+      // Add bonuses from previous frame
+      if (isStrike) {
+        totalScore += (firstRoll + secondRoll);
+      } else if (isSpare) {
+        totalScore += firstRoll;
+      }
+      
+      // Calculate whether there is strike or spare that will be utilized in next frame.
+      isStrike = false;
+      isSpare = false;
+
+      if ((firstRoll === 10 && secondRoll === 0)
+        || (firstRoll === 0 && secondRoll === 10)) {
+        isStrike = true;
+      } else if (firstRoll + secondRoll === 10) {
+        isSpare = true;
+      }
+    }
+
+    return totalScore;
   }
 
+  /** Checks whether argument provided is of a number type. */
   private isNumeric(possibleNumber?: string | number): boolean {
     return possibleNumber != null
       && possibleNumber !== ''
