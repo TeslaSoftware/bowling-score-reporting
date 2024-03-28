9b6b7c7d1d1bb0ac83a766a2df0db4a8bbf7cb70
Sebastian <stysler@gmail.com>
2024-03-28 21:13:18 +0100

    Implement parsing and vaidating data

--- src/app/ibowling-result.ts
+++ src/app/ibowling-result.ts
@@ -1,17 +1,6 @@
-interface Frame {
-    readonly rolls: [number, number];
-    readonly frameScore: number;
-}
-
-type Tuple<
-  T,
-  N extends number,
-  R extends readonly T[] = [],
-> = R['length'] extends N ? R : Tuple<T, N, readonly [T, ...R]>;
-
 export interface IBowlingResult {
     readonly playerName: string;
-    readonly frames: Tuple<Frame, 11>;
+    readonly scores: number[];
     readonly totalScore: number;
 }
 
--- src/app/parsing-service.service.ts
+++ src/app/parsing-service.service.ts
@@ -8,16 +8,101 @@ import { of, Observable, BehaviorSubject } from 'rxjs';
 export class ParsingService {
   private bowlingResultsSubject: BehaviorSubject<IBowlingResult[]> = new BehaviorSubject(new Array<IBowlingResult>());
 
-  parseTextToData(textData: string): Observable<boolean> {
-    if (!textData) {
+  parseTextToData(rawTextData: string): Observable<boolean> {
+    if (!rawTextData) {
       return of(false);
     }
-    console.log(textData);
+    console.log(rawTextData);
 
-    return of(true);
+    return of(this.performParsing(rawTextData));
+  }
+
+  /** Pefroms parsing, updates data and returns whether parsing was successful. */
+  private performParsing(rawTextData: string): boolean {
+    let bowlingResults: IBowlingResult[] = [];
+    try {
+      bowlingResults = this.parseAndValidateRawText(rawTextData);
+    } catch(error: unknown) {
+      let errorMessage;
+      if (typeof error === "string") {
+        errorMessage = error.toUpperCase() // works, `e` narrowed to string
+      } else if (error instanceof Error) {
+        errorMessage = error.message
+      }
+      console.log("Parsing failed due to " + errorMessage);
+      return false;
+    } finally{
+      this.bowlingResultsSubject.next(bowlingResults);
+    }
+    
+    console.log("Parsing was successful!!");
+    return true;
   }
 
   getBowlingResults(): BehaviorSubject<IBowlingResult[]> {
     return this.bowlingResultsSubject;
+  }
+
+  /** 
+   * Attempts to parse and validate raw text to expected data format.
+   * Throws an error when invalid data is encountered.
+   */
+  private parseAndValidateRawText(rawText: string): IBowlingResult[] {
+    const textLines = rawText.trim().split(/\r?\n/);
+    if (textLines.length % 2 !== 0) {
+      return [];
+    }
+
+    const results = [];
+
+    for (let i = 0; i + 1 < textLines.length; i += 2) {
+      const playerName = textLines[i].trim();
+      const scores = this.parseAndValidateScoresString(textLines[i + 1]);
+      const totalScore = this.calculateTotalScore(scores);
+
+      const bowlingResult:IBowlingResult = {
+        playerName, scores, totalScore
+      };
+
+      results.push(bowlingResult);
+    }
+
+    return results;
+  }
+
+  /** Parses and validates scores string. */
+  private parseAndValidateScoresString(scoresLine: string): number[] {
+    const scoresStrings = scoresLine.split(",").map(str => str.trim())
+
+    if(scoresStrings.some(item => !this.isNumeric(item))){
+      throw new Error("Nieprawidłowy format! Przynajmniej jeden z rzutów jest nie jest numerem");
+    }
+
+    const numericScores = scoresStrings.map(str => parseInt(str));
+
+    if(numericScores.length < 20 || numericScores.length > 21){
+      throw new Error("Nieprawidłowy format. Oczekiwano numer rzutów 20 lub 21 a było " + numericScores.length);
+    }
+
+    for (let i = 0; i + 1 < numericScores.length; i += 2) {
+      const scoreForBothRolls = numericScores[i] + numericScores[i + 1]
+      if(scoreForBothRolls > 10) {
+        throw new Error("Nieprawidłowy format. W jednej rundzie można maksymalnie zdobyć 10 punktów a zawodnik zdobył " + scoreForBothRolls);
+      }
+    }
+
+    return numericScores; 
+  }
+
+  /** Calculates total score based on the game rules. */
+  private calculateTotalScore(scores: number[]): number {
+    // TODO: implement this menthod
+    return 999;
+  }
+
+  private isNumeric(possibleNumber?: string | number): boolean {
+    return possibleNumber != null
+      && possibleNumber !== ''
+      && !isNaN(Number(possibleNumber));
   }
 }
